ec6c762e3d8eabfb6cdf7dde4bfdd570a002f9a8
rbnset <rbn.setiyawan@gmail.com>
2026-08-13 16:23:47 +0700

    feature: add notification popup message

--- app/Filament/Resources/Rws/Pages/CreateRw.php
+++ app/Filament/Resources/Rws/Pages/CreateRw.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\Rws\Pages;
 
 use App\Filament\Resources\Rws\RwResource;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateRw extends CreateRecord
@@ -22,5 +23,16 @@ class CreateRw extends CreateRecord
     protected function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('index');
+    }
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('RW berhasil ditambahkan')
+            ->body(
+                "RW {$this->record->number} berhasil ditambahkan ke data wilayah."
+            )
+            ->duration(5000);
     }
 }
--- app/Filament/Resources/Rws/Pages/EditRw.php
+++ app/Filament/Resources/Rws/Pages/EditRw.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\Rws\Pages;
 
 use App\Filament\Resources\Rws\RwResource;
 use Filament\Actions\DeleteAction;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 
 class EditRw extends EditRecord
@@ -13,12 +14,37 @@ class EditRw extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            DeleteAction::make(),
+            DeleteAction::make()
+                ->label('Hapus')
+                ->modalHeading(fn() => "Hapus RW {$this->record->number}?")
+                ->modalDescription(
+                    fn() =>
+                    "RW {$this->record->number} akan dihapus dari data wilayah. "
+                        . "Tindakan ini tidak dapat dibatalkan."
+                )
+                ->modalSubmitActionLabel('Ya, hapus')
+                ->successNotification(
+                    Notification::make()
+                        ->success()
+                        ->title('RW berhasil dihapus')
+                        ->body("RW {$this->record->number} telah dihapus dari data wilayah.")
+                ),
         ];
     }
 
     protected function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('index');
+    }
+
+    protected function getSavedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('RW berhasil diperbarui')
+            ->body(
+                "Data RW {$this->record->number} berhasil diperbarui."
+            )
+            ->duration(5000);
     }
 }
